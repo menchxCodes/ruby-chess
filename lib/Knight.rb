47d3600024be92683bf606ffd7890a6a7724ad73
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-16 02:11:02 +0330

    Add Knight class & legal moves logic

---
+++ lib/Knight.rb
@@ -0,0 +1,90 @@
+require './lib/Board.rb'
+require './lib/Pawn.rb'
+require './lib/Bishop.rb'
+require './lib/Rook.rb'
+require './lib/Queen.rb'
+require './lib/Piece.rb'
+
+class WhiteKnight < White
+  attr_reader :avatar, :start_pos
+  attr_accessor :moves
+
+  def initialize(x_pos, y_pos)
+    @avatar = "\u2658"
+    @start_pos = [x_pos, y_pos]
+    @current_pos = @start_pos
+    @moves = []
+  end
+
+  def legal_moves(board)
+    @legal_moves = []
+
+    move = [@current_pos[0] + 1, @current_pos[1] + 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] + 1, @current_pos[1] - 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] - 1, @current_pos[1] + 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] - 1, @current_pos[1] - 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] + 2, @current_pos[1] + 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] + 2, @current_pos[1] - 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] - 2, @current_pos[1] + 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    move = [@current_pos[0] - 2, @current_pos[1] - 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(White)
+
+    @legal_moves
+  end
+end
+
+class BlackKnight < Black
+  attr_reader :avatar, :start_pos
+  attr_accessor :moves
+
+  def initialize(x_pos, y_pos)
+    @avatar = "\u265e"
+    @start_pos = [x_pos, y_pos]
+    @current_pos = @start_pos
+    @moves = []
+  end
+
+  def legal_moves(board)
+    @legal_moves = []
+
+    move = [@current_pos[0] + 1, @current_pos[1] + 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] + 1, @current_pos[1] - 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] - 1, @current_pos[1] + 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] - 1, @current_pos[1] - 2]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] + 2, @current_pos[1] + 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] + 2, @current_pos[1] - 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] - 2, @current_pos[1] + 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    move = [@current_pos[0] - 2, @current_pos[1] - 1]
+    @legal_moves.push(move) unless !within_bound?(move) || board.piece_at(move[0], move[1]).is_a?(Black)
+
+    @legal_moves
+  end
+end
