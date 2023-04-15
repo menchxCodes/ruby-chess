65964e322c41aef82deee5274184b7c942530eac
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-16 02:50:59 +0330

    Add #calculate_legals and finish initial piece setup

--- lib/Board.rb
+++ lib/Board.rb
@@ -6,10 +6,24 @@ require './lib/Knight.rb'
 require './lib/Piece.rb'
 
 class Board
-  attr_accessor :board
+  attr_accessor :board, :player_one, :player_two
 
   def initialize
     @board = create_board
+    @player_one = White.new
+    @player_two = Black.new
+  end
+
+  def calculate_legals(player)
+    output = []
+    player.pieces.each do |piece|
+      legals = Array.new(2) {Array.new}
+      moves = piece.legal_moves(self)
+      legals[0] = piece.start_pos unless moves.empty?
+      legals[1].replace(moves) unless moves.empty?
+      output << legals unless moves.empty?
+    end
+    output
   end
 
   def create_board
@@ -46,7 +60,11 @@ class Board
   end
 
   def setup_pieces
+
+    setup_white_pieces
     setup_white_pawns
+
+    setup_black_pieces
     setup_black_pawns
   end
 
@@ -54,11 +72,37 @@ class Board
     @board[2].each_index do |index|
       @board[2][index] = WhitePawn.new(index, 2) unless index.zero?
     end
+    @board[2].each_with_index { |piece, index| @player_one.pieces.push(piece) unless index.zero? || piece == ' ' }
+  end
+
+  def setup_white_pieces
+    @board[1][1] = WhiteRook.new(1, 1)
+    @board[1][2] = WhiteKnight.new(2, 1)
+    @board[1][3] = WhiteBishop.new(3, 1)
+    @board[1][4] = WhiteQueen.new(4, 1)
+    # @board[1][5] = WhiteKing.new(5, 1)
+    @board[1][6] = WhiteBishop.new(6, 1)
+    @board[1][7] = WhiteKnight.new(7, 1)
+    @board[1][8] = WhiteRook.new(8, 1)
+    @board[1].each_with_index { |piece, index| @player_one.pieces.push(piece) unless index.zero? || piece == ' ' }
   end
 
   def setup_black_pawns
     @board[7].each_index do |index|
       @board[7][8 - index] = BlackPawn.new(8 - index, 7) unless index == 8
     end
+    @board[7].each_with_index { |piece, index| @player_two.pieces.push(piece) unless index.zero? || piece == ' ' }
+  end
+
+  def setup_black_pieces
+    @board[8][1] = BlackRook.new(1, 8)
+    @board[8][2] = BlackKnight.new(2, 8)
+    @board[8][3] = BlackBishop.new(3, 8)
+    @board[8][4] = BlackQueen.new(4, 8)
+    # @board[1][5] = BlackKing.new(5, 8)
+    @board[8][6] = BlackBishop.new(6, 8)
+    @board[8][7] = BlackKnight.new(7, 8)
+    @board[8][8] = BlackRook.new(8, 8)
+    @board[8].each_with_index { |piece, index| @player_two.pieces.push(piece) unless index.zero? || piece == ' ' }
   end
 end
