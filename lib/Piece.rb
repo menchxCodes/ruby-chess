4761a64efe677f71991d4f34e301ebc79894b3e9
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-16 01:13:12 +0330

    Add #within_bound(move) method, and variable for lost pieces

--- lib/Piece.rb
+++ lib/Piece.rb
@@ -1,21 +1,30 @@
 require './lib/Board.rb'
 require './lib/Pawn.rb'
+require './lib/Rook.rb'
+require './lib/Bishop.rb'
 
 class Piece
     def piece?
         return self.kind_of?(Piece)
+    end
+
+    def within_bound?(move)
+      return true if move[0].between?(1, 8) && move[1].between?(1, 8)
+      false
     end
 end
 
 class White < Piece
     def initialize
         @white_pieces = []
+        @white_lost = []
     end
 end
 
 class Black < Piece
     def initialize
         @black_pieces = []
+        @black_lost = []
     end
 end
 
