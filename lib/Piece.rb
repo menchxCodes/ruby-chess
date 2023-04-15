80f7e4f33a98cdaaf9b3c36d0432116909ed4278
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-16 02:10:51 +0330

    formatting

--- lib/Piece.rb
+++ lib/Piece.rb
@@ -1,30 +1,32 @@
-require './lib/Board.rb'
-require './lib/Pawn.rb'
-require './lib/Rook.rb'
-require './lib/Bishop.rb'
+require './lib/Board'
+require './lib/Pawn'
+require './lib/Rook'
+require './lib/Bishop'
+require './lib/Queen.rb'
+require './lib/Knight.rb'
 
 class Piece
-    def piece?
-        return self.kind_of?(Piece)
-    end
+  def piece?
+    is_a?(Piece)
+  end
 
-    def within_bound?(move)
-      return true if move[0].between?(1, 8) && move[1].between?(1, 8)
-      false
-    end
+  def within_bound?(move)
+    return true if move[0].between?(1, 8) && move[1].between?(1, 8)
+
+    false
+  end
 end
 
 class White < Piece
-    def initialize
-        @white_pieces = []
-        @white_lost = []
-    end
+  def initialize
+    @white_pieces = []
+    @white_lost = []
+  end
 end
 
 class Black < Piece
-    def initialize
-        @black_pieces = []
-        @black_lost = []
-    end
+  def initialize
+    @black_pieces = []
+    @black_lost = []
+  end
 end
-
