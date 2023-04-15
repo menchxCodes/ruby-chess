4fa13a4e5a2ec8aab60ab16d9334185152f8eb87
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-16 00:15:54 +0330

    Add #piece_at & #setup methods for pawns

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,3 +1,6 @@
+require './lib/Pawn.rb'
+require './lib/Piece.rb'
+
 class Board
   attr_accessor :board
 
@@ -6,7 +9,8 @@ class Board
   end
 
   def create_board
-    string = ' abcdefgh'
+    # string = ' abcdefgh'
+    string = ' 12345678'
     new_board = Array.new(9) { Array.new(9) }
     new_board.each_with_index do |row, row_index|
       row.each_with_index do |_col, col_index|
@@ -26,10 +30,33 @@ class Board
     @board.reverse.each do |row|
       string = ''
       row.each do |col|
-        string.concat(" #{col} |")
+        col.kind_of?(Piece) ? string.concat(" #{col.avatar} |"): string.concat(" #{col} |")
       end
       puts string
       puts '------------------------------------'
     end
   end
+
+  def piece_at(x_pos, y_pos)
+    @board[y_pos][x_pos]
+  end
+
+  def setup_pieces
+    setup_white_pawns
+    setup_black_pawns
+  end
+
+  def setup_white_pawns
+    @board[2].each_index do |index|
+      @board[2][index] = WhitePawn.new(index, 2) unless index.zero?
+    end
+  end
+
+  def setup_black_pawns
+    @board[7].each_index do |index|
+      @board[7][8 - index] = BlackPawn.new(8 - index, 7) unless index == 8
+    end
+  end
 end
+
+
