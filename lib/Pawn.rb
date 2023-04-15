4054399b8bf20e9c44630c2f8e3de7c0a5492486
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-16 00:17:33 +0330

    Add Pawn class & legal_moves logic for b&w pawns

---
+++ lib/Pawn.rb
@@ -0,0 +1,84 @@
+require './lib/Board.rb'
+require './lib/Piece.rb'
+
+class WhitePawn < White
+    attr_reader :avatar, :start_pos
+    attr_accessor :moves
+  def initialize(x_pos, y_pos)
+    @avatar = "\u2659"
+    @start_pos = [x_pos, y_pos]
+    @current_pos = @start_pos
+    @moves = []
+  end
+
+  def legal_moves(board)
+    @legal_moves = []
+    #single-forward_move
+    unless board.piece_at(@current_pos[0], @current_pos[1] + 1).kind_of?(Piece) && @current_pos[1] != 8
+      puts 'single-forward_move'
+      @legal_moves.push([@current_pos[0], @current_pos[1] + 1])
+    end
+
+    #double-forward-move
+    blocked_forward = board.piece_at(@current_pos[0], @current_pos[1] + 1).kind_of?(Piece) || board.piece_at(@current_pos[0], @current_pos[1] + 2).kind_of?(Piece)
+    if @moves.empty? && !blocked_forward
+      puts 'double-forward-move'
+      @legal_moves.push([@current_pos[0], @current_pos[1] + 2])
+    end
+
+    #left-attack
+    if board.piece_at(@current_pos[0] - 1, @current_pos[1] + 1).kind_of?(Black) && @current_pos[0] != 1
+      puts 'left-attack'
+      @legal_moves.push([@current_pos[0] - 1, @current_pos[1] + 1])
+    end
+
+    #right-attack
+    if board.piece_at(@current_pos[0] + 1, @current_pos[1] + 1).kind_of?(Black) && @current_pos[0] != 8
+      puts 'right-attack'
+      @legal_moves.push([@current_pos[0] + 1, @current_pos[1] + 1])
+    end
+    
+    @legal_moves
+  end
+end
+
+class BlackPawn < Black
+    attr_reader :avatar, :start_pos
+    attr_accessor :moves
+    def initialize(x_pos, y_pos)
+        @avatar = "\u265f"
+        @start_pos = [x_pos, y_pos]
+        @current_pos = @start_pos
+        @moves = []
+    end
+
+    def legal_moves(board)
+      @legal_moves = []
+      #single-forward_move
+      unless board.piece_at(@current_pos[0], @current_pos[1] - 1).kind_of?(Piece) && @current_pos[1] != 1
+        puts 'single-forward_move'
+        @legal_moves.push([@current_pos[0], @current_pos[1] - 1])
+      end
+
+      #double-forward-move
+      blocked_forward = board.piece_at(@current_pos[0], @current_pos[1] - 1).kind_of?(Piece) || board.piece_at(@current_pos[0], @current_pos[1] - 2).kind_of?(Piece)
+      if @moves.empty? && !blocked_forward
+        puts 'double-forward-move'
+        @legal_moves.push([@current_pos[0], @current_pos[1] - 2])
+      end
+
+      #left-attack
+      if board.piece_at(@current_pos[0] - 1, @current_pos[1] - 1).kind_of?(White) && @current_pos[0] != 1
+        puts 'left-attack'
+        @legal_moves.push([@current_pos[0] - 1, @current_pos[1] - 1])
+      end
+
+      #right-attack
+      if board.piece_at(@current_pos[0] + 1, @current_pos[1] - 1).kind_of?(White) && @current_pos[0] != 8
+        puts 'right-attack'
+        @legal_moves.push([@current_pos[0] + 1, @current_pos[1] - 1])
+      end
+      
+      @legal_moves
+    end
+end
