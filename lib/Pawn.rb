57ca4f62532e4f2c43e822f67bd4f6d9dc557a2a
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-17 23:29:33 +0330

    Add logic to avoid taking the opposite king

--- lib/Pawn.rb
+++ lib/Pawn.rb
@@ -1,9 +1,10 @@
-require './lib/Board.rb'
-require './lib/Bishop.rb'
-require './lib/Rook.rb'
-require './lib/Queen.rb'
-require './lib/Piece.rb'
-require './lib/Knight.rb'
+require './lib/Board'
+require './lib/Bishop'
+require './lib/Rook'
+require './lib/Queen'
+require './lib/King'
+require './lib/Piece'
+require './lib/Knight'
 
 class WhitePawn < White
   attr_reader :avatar, :start_pos
@@ -20,25 +21,34 @@ class WhitePawn < White
     @legal_moves = []
     # single-forward_move
     return @legal_moves if @current_pos[1] == 8
-    unless board.piece_at(@current_pos[0], @current_pos[1] + 1).kind_of?(Piece) && @current_pos[1] != 8
+
+    if !board.piece_at(@current_pos[0],
+                       @current_pos[1] + 1).is_a?(Piece) && !@current_pos[1] != 8 && !board.piece_at(@current_pos[0],
+                                                                                                     @current_pos[1] + 1).is_a?(King)
       # puts 'single-forward_move'
       @legal_moves.push([@current_pos[0], @current_pos[1] + 1])
     end
 
     # double-forward-move
     if @moves.empty?
-      blocked_forward = board.piece_at(@current_pos[0], @current_pos[1] + 1).kind_of?(Piece) || board.piece_at(@current_pos[0], @current_pos[1] + 2).kind_of?(Piece)
+      blocked_forward = board.piece_at(@current_pos[0],
+                                       @current_pos[1] + 1).is_a?(Piece) || board.piece_at(@current_pos[0],
+                                                                                           @current_pos[1] + 2).is_a?(Piece)
       @legal_moves.push([@current_pos[0], @current_pos[1] + 2]) unless blocked_forward
     end
 
-    #left-attack
-    if board.piece_at(@current_pos[0] - 1, @current_pos[1] + 1).kind_of?(Black) && @current_pos[0] != 1
+    # left-attack
+    if board.piece_at(@current_pos[0] - 1,
+                      @current_pos[1] + 1).is_a?(Black) && !board.piece_at(@current_pos[0] - 1,
+                                                                           @current_pos[1] + 1).is_a?(King) && @current_pos[0] != 1
       # puts 'left-attack'
       @legal_moves.push([@current_pos[0] - 1, @current_pos[1] + 1])
     end
 
     # right-attack
-    if board.piece_at(@current_pos[0] + 1, @current_pos[1] + 1).kind_of?(Black) && @current_pos[0] != 8
+    if board.piece_at(@current_pos[0] + 1,
+                      @current_pos[1] + 1).is_a?(Black) && board.piece_at(@current_pos[0] + 1,
+                                                                          @current_pos[1] + 1).is_a?(King) && @current_pos[0] != 8
       # puts 'right-attack'
       @legal_moves.push([@current_pos[0] + 1, @current_pos[1] + 1])
     end
@@ -63,25 +73,33 @@ class BlackPawn < Black
     return @legal_moves if @current_pos[1] == 1
 
     # single-forward_move
-    unless board.piece_at(@current_pos[0], @current_pos[1] - 1).kind_of?(Piece) && @current_pos[1] != 1
+    if !board.piece_at(@current_pos[0],
+                       @current_pos[1] - 1).is_a?(Piece) && !@current_pos[1] != 1 && !board.piece_at(@current_pos[0],
+                                                                                                     @current_pos[1] - 1).is_a?(King)
       # puts 'single-forward_move'
       @legal_moves.push([@current_pos[0], @current_pos[1] - 1])
     end
 
     # double-forward-move
     if @moves.empty?
-      blocked_forward = board.piece_at(@current_pos[0], @current_pos[1] - 1).kind_of?(Piece) || board.piece_at(@current_pos[0], @current_pos[1] - 2).kind_of?(Piece)
+      blocked_forward = board.piece_at(@current_pos[0],
+                                       @current_pos[1] - 1).is_a?(Piece) || board.piece_at(@current_pos[0],
+                                                                                           @current_pos[1] - 2).is_a?(Piece)
       @legal_moves.push([@current_pos[0], @current_pos[1] - 2]) unless blocked_forward
     end
 
     # left-attack
-    if board.piece_at(@current_pos[0] - 1, @current_pos[1] - 1).kind_of?(White) && @current_pos[0] != 1
+    if board.piece_at(@current_pos[0] - 1,
+                      @current_pos[1] - 1).is_a?(White) && board.piece_at(@current_pos[0] - 1,
+                                                                          @current_pos[1] - 1).is_a?(King) && @current_pos[0] != 1
       # puts 'left-attack'
       @legal_moves.push([@current_pos[0] - 1, @current_pos[1] - 1])
     end
 
     # right-attack
-    if board.piece_at(@current_pos[0] + 1, @current_pos[1] - 1).kind_of?(White) && @current_pos[0] != 8
+    if board.piece_at(@current_pos[0] + 1,
+                      @current_pos[1] - 1).is_a?(White) && board.piece_at(@current_pos[0] + 1,
+                                                                          @current_pos[1] - 1).is_a?(King) && @current_pos[0] != 8
       # puts 'right-attack'
       @legal_moves.push([@current_pos[0] + 1, @current_pos[1] - 1])
     end
