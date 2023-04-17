eee6f647a31c958a743e22a786ef5433ebb26071
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-17 23:31:30 +0330

    Implement King and stopping loop when just kings are left

--- lib/Board.rb
+++ lib/Board.rb
@@ -3,6 +3,7 @@ require './lib/Rook.rb'
 require './lib/Bishop.rb'
 require './lib/Queen.rb'
 require './lib/Knight.rb'
+require './lib/King.rb'
 require './lib/Piece.rb'
 
 class Board
@@ -13,15 +14,20 @@ class Board
     @player_one = White.new
     @player_two = Black.new
     @current_player = @player_one
+    @turn = 0
+    @temp = ""
   end
 
   def random_loop
-    loop_count = 0
     until calculate_legals(@current_player).empty?
-      loop_count += 1
+      break if @current_player.pieces.size == 1 && @current_player.pieces[0].is_a?(King)
+
+      break if opposite_player.pieces.size == 1 && opposite_player.pieces[0].is_a?(King)
+
+      @turn += 1
       do_random_legal_move(@current_player)
       print_board
-      puts loop_count
+      puts @turn
       change_player
       puts "#{@current_player} turn"
     end
@@ -38,6 +44,7 @@ class Board
 
   def opposite_player(player = @current_player)
     return @player_one if player == @player_two
+
     @player_two
   end
 
@@ -66,7 +73,12 @@ class Board
 
     piece = piece_at(random_piece[0], random_piece[1])
     piece.moves.push(random_move)
-    target_piece = piece_at(random_move[0],random_move[1])
+    target_piece = piece_at(random_move[0], random_move[1])
+
+    if target_piece.is_a?(King)
+      @temp.concat("KING DELETED #{@turn} |")
+    end
+
     if target_piece.is_a?(Piece)
       opposite_player.lost << target_piece
       opposite_player.pieces.delete(target_piece)
@@ -98,7 +110,7 @@ class Board
     @board.reverse.each do |row|
       string = ''
       row.each do |col|
-        col.kind_of?(Piece) ? string.concat(" #{col.avatar} |"): string.concat(" #{col} |")
+        col.is_a?(Piece) || col.is_a?(King) ? string.concat(" #{col.avatar} |"): string.concat(" #{col} |")
       end
       puts string
       puts '------------------------------------'
@@ -110,6 +122,7 @@ class Board
     black_lost = "black-lost:"
     @player_two.lost.each {|piece| black_lost.concat(piece.avatar) }
     puts black_lost
+    puts @temp
   end
 
   def piece_at(x_pos, y_pos)
@@ -117,7 +130,6 @@ class Board
   end
 
   def setup_pieces
-
     setup_white_pieces
     setup_white_pawns
 
@@ -137,18 +149,18 @@ class Board
     @board[1][2] = WhiteKnight.new(2, 1)
     @board[1][3] = WhiteBishop.new(3, 1)
     @board[1][4] = WhiteQueen.new(4, 1)
-    # @board[1][5] = WhiteKing.new(5, 1)
+    @board[1][5] = WhiteKing.new(5, 1)
     @board[1][6] = WhiteBishop.new(6, 1)
     @board[1][7] = WhiteKnight.new(7, 1)
     @board[1][8] = WhiteRook.new(8, 1)
-    @board[1].each_with_index { |piece, index| @player_one.pieces.push(piece) unless index.zero? || piece == ' ' }
+    @board[1].each_with_index { |piece, index| @player_one.pieces.push(piece) unless index.zero? }
   end
 
   def setup_black_pawns
     @board[7].each_index do |index|
       @board[7][8 - index] = BlackPawn.new(8 - index, 7) unless index == 8
     end
-    @board[7].each_with_index { |piece, index| @player_two.pieces.push(piece) unless index.zero? || piece == ' ' }
+    @board[7].each_with_index { |piece, index| @player_two.pieces.push(piece) unless index.zero? }
   end
 
   def setup_black_pieces
@@ -156,10 +168,10 @@ class Board
     @board[8][2] = BlackKnight.new(2, 8)
     @board[8][3] = BlackBishop.new(3, 8)
     @board[8][4] = BlackQueen.new(4, 8)
-    # @board[1][5] = BlackKing.new(5, 8)
+    @board[8][5] = BlackKing.new(5, 8)
     @board[8][6] = BlackBishop.new(6, 8)
     @board[8][7] = BlackKnight.new(7, 8)
     @board[8][8] = BlackRook.new(8, 8)
-    @board[8].each_with_index { |piece, index| @player_two.pieces.push(piece) unless index.zero? || piece == ' ' }
+    @board[8].each_with_index { |piece, index| @player_two.pieces.push(piece) unless index.zero? }
   end
 end
