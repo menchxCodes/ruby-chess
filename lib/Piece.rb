e977f02037884e94058d7b02547f1d398d8325f9
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-18 13:58:37 +0330

    Move #white_piece? & #black_piece? from Board to Piece class

--- lib/Piece.rb
+++ lib/Piece.rb
@@ -6,15 +6,21 @@ require './lib/Queen.rb'
 require './lib/King.rb'
 require './lib/Knight.rb'
 module Boundries
+  # Returns TRUE if a move is within the boundries of the chess board.
+  # move = [a,b], a & b are both between 1 and 8 => TRUE
   def within_bound?(move)
     return true if move[0].between?(1, 8) && move[1].between?(1, 8)
 
     false
-  end 
+  end
 end
 class Piece
-  def piece?
-    is_a?(Piece)
+  def white_piece?(piece)
+    piece.is_a?(White) || piece.is_a?(King)
+  end
+
+  def black_piece?(piece)
+    piece.is_a?(Black) || piece.is_a?(King)
   end
 end
 
