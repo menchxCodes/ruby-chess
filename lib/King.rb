38c371e57e3c080d5abc7d4210bdb098d024a7bc
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-20 17:14:14 +0330

    update King #legal_moves to avoid squares under other king's influence

--- lib/King.rb
+++ lib/King.rb
@@ -36,7 +36,25 @@ class WhiteKing < King
       @legal_moves.push(ghost_move) unless !within_bound?(ghost_move) || white_piece?(target)
     end
 
+    # removes moves around the other king
+    black_king = board.find_black_king
+    black_king_moves = black_king.influence
+    black_king_moves.each do |move|
+      @legal_moves.delete(move) if black_king_moves.include?(move)
+    end
+
     @legal_moves
+  end
+
+  def influence
+    influence = []
+    king_moves = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+
+    king_moves.each do |move|
+      ghost_move = [@current_pos[0] + move[0], @current_pos[1] + move[1]]
+      influence.push(ghost_move) if within_bound?(ghost_move)
+    end
+    influence
   end
 end
 
@@ -61,6 +79,24 @@ class BlackKing < King
       @legal_moves.push(ghost_move) unless !within_bound?(ghost_move) || black_piece?(target)
     end
 
+    # removes moves around the other king
+    white_king = board.find_white_king
+    white_king_moves = white_king.influence
+    white_king_moves.each do |move|
+      @legal_moves.delete(move) if white_king_moves.include?(move)
+    end
+
     @legal_moves
+  end
+
+  def influence
+    influence = []
+    king_moves = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+
+    king_moves.each do |move|
+      ghost_move = [@current_pos[0] + move[0], @current_pos[1] + move[1]]
+      influence.push(ghost_move) if within_bound?(ghost_move)
+    end
+    influence
   end
 end
