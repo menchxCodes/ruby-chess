30028d386e869162e2b79bc0a316448515d96324
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-25 17:19:24 +0330

    Add #check_moves to Knight class

--- lib/Knight.rb
+++ lib/Knight.rb
@@ -29,6 +29,27 @@ class WhiteKnight < White
 
     @legal_moves
   end
+
+  def check_moves(board)
+    check_moves = []
+    knight_moves = [[-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1]]
+
+    black_king_pos = board.find_black_king.current_pos
+
+    knight_moves.each do |move|
+      ghost_move = [@current_pos[0] + move[0], @current_pos[1] + move[1]]
+      target = board.piece_at(ghost_move[0], ghost_move[1]) if within_bound?(ghost_move)
+      until !within_bound?(ghost_move) || white_piece_only?(target)
+        check_moves.push(ghost_move) if ghost_move == black_king_pos
+        break if target.is_a?(Black)
+
+        ghost_move = [ghost_move[0] + move[0], ghost_move[1] + move[1]]
+        target = board.piece_at(ghost_move[0], ghost_move[1]) if within_bound?(ghost_move)
+      end
+    end
+
+    check_moves
+  end
 end
 
 class BlackKnight < Black
@@ -53,5 +74,26 @@ class BlackKnight < Black
     end
 
     @legal_moves
+  end
+
+  def check_moves(board)
+    check_moves = []
+    knight_moves = [[-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1]]
+
+    white_king_pos = board.find_white_king.current_pos
+
+    knight_moves.each do |move|
+      ghost_move = [@current_pos[0] + move[0], @current_pos[1] + move[1]]
+      target = board.piece_at(ghost_move[0], ghost_move[1]) if within_bound?(ghost_move)
+      until !within_bound?(ghost_move) || black_piece_only?(target)
+        check_moves.push(ghost_move) if ghost_move == white_king_pos
+        break if target.is_a?(White)
+
+        ghost_move = [ghost_move[0] + move[0], ghost_move[1] + move[1]]
+        target = board.piece_at(ghost_move[0], ghost_move[1]) if within_bound?(ghost_move)
+      end
+    end
+
+    check_moves
   end
 end
